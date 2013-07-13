1252ae012086de8229e8fe03fe027b4a5c994622
Ubuntu <ubuntu@ip-172-31-29-216.us-west-2.compute.internal>
2013-07-13 22:29:49 +0000

    Modified web.js to read content from index.html

--- web.js
+++ web.js
@@ -1,10 +1,24 @@
+var fs = require('fs');
 var express = require('express');
+
+var responseWriter = function(httpResponse, bufferData){
+			var data = bufferData.toString('utf-8');
+			httpResponse.send(data);
+		     }
 
 var app = express.createServer(express.logger());
 
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var result = 'abiti';
+	fs.readFile('index.html',function (err, data) {
+				        if (err) throw err;
+				        responseWriter(response, data);
+				        }
+	);
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
