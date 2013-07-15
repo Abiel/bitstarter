789492802ffcfcd6b635ee8bdbbfae4f2ed7d203
Abiel <abiel.m.woldu@gmail.com>
2013-07-15 04:58:37 +0000

    Added the functionality to receive urls and check content after making a rest.get() call.

--- grader.js
+++ grader.js
@@ -26,23 +26,31 @@ var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var rest = require('restler');
+var Type = require('type-of-is');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var URL_DEFAULT = "http://thawing-brushlands-1365.herokuapp.com/";
+var URL_DEFAULT = "http://thawing-brushlands-1365.herokuapp.com";
 
-var getURLContent = function(url){
-    rest.get(url).on('complete', processResponse);
+
+var assertValidURL = function(url){
+    return url;
 }
 
-var processResponse = function(result) {
+var getURLContent = function(url, checkFile){
+    rest.get(url).on('complete',  function(result) {
     if (result instanceof Error) {
 	console.log('Error: ' + result.message);
 	process.exit(1);
     } else {
-	console.log("URL -->" + result);
+	//console.log("URL -->" + result);
+	 var checkJson = checkHtmlcontent(result, checkFile);
+         var outJson = JSON.stringify(checkJson, null, 4);
+         console.log(outJson);
 	return result;
     }
-}
+})
+
+};
 
 
 var assertFileExists = function(infile) {
@@ -51,18 +59,29 @@ var assertFileExists = function(infile) {
         console.log("%s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
-    return instr;
+//    console.log("assert returning-->"+instr);
+   return instr;
 };
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
+//    console.log("content of html file: " + htmlfile);
+//    return htmlfile;
 };
+
+
+var cheerioHtmlcontent = function(htmlcontent) {
+    return cheerio.load(htmlcontent);
+};
+
+
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
+    //console.log("typeis:" + Type(htmlfile));
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
@@ -74,21 +93,68 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+
+var checkHtmlcontent = function(htmlcontent, checksfile) {
+    $ = cheerioHtmlcontent(htmlcontent);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        //console.log('checking-->' + checks[ii] + "found: " + $(checks[ii]).length);
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
+
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+
+var contains = function(array, value){
+
+    array.forEach(function(val, index, array) {
+	console.log(index + ': ' + val);
+	if(val==value){
+	    return true;
+	    }
+    });
+
+}; 
+
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <url_content>', 'Path to url', clone(getURLContent), URL_DEFAULT)
+        .option('-u, --url <url_content>', 'Path to url', clone(assertValidURL), URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+process.argv.forEach(function(val, index, array) {
+
+        if(val=="--file"){
+	    //call is made for file input 
+	    var checkJson = checkHtmlFile(program.file, program.checks);
+	    var outJson = JSON.stringify(checkJson, null, 4);
+	    console.log(outJson);
+	}else if(val=="--url"){
+	    //call is made for uri input.
+	    getURLContent(program.url, program.checks);
+	}
+});
+
+
+    //console.log("program.file-->" + program.file);
+    //console.log("program.checks-->" + program.checks);
+    //console.log("program.url-->" + program.url);
+    //console.log("is file-->" + contains(process.argv, "--file")); 
+    //var checkJson = checkHtmlFile(program.file, program.checks);
+    //var outJson = JSON.stringify(checkJson, null, 4);
+    //console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
