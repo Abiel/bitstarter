e677da528716ce708a62f1c4adf2ebe809202676
Abiel <abiel.m.woldu@gmail.com>
2013-07-15 03:16:02 +0000

    Adding restler module for rest calls.

--- grader.js
+++ grader.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
@@ -24,8 +25,25 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://thawing-brushlands-1365.herokuapp.com/";
+
+var getURLContent = function(url){
+    rest.get(url).on('complete', processResponse);
+}
+
+var processResponse = function(result) {
+    if (result instanceof Error) {
+	console.log('Error: ' + result.message);
+	process.exit(1);
+    } else {
+	console.log("URL -->" + result);
+	return result;
+    }
+}
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -66,6 +84,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_content>', 'Path to url', clone(getURLContent), URL_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
